290da9f973248103c943f1372544c28a926c438c
cole <cole@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-08-16 22:33:53 +0000

    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/fundraising@6854 f53fe46e-8c02-459b-9fdd-93e75771b278

--- fundraiser.php
+++ fundraiser.php
@@ -3,7 +3,7 @@
 Plugin Name: Fundraising
 Plugin URI: http://premium.wpmudev.org/project/fundraising/
 Description: Create a fundraising page for any purpose or project.
-Version: 2.1.6
+Version: 2.1.7
 Text Domain: wdf
 Author: Cole (Incsub)
 Author URI: http://premium.wpmudev.org/
@@ -83,7 +83,7 @@ class WDF {
 		$this->_construct();
 	}
 	function _vars() {
-		$this->version = '2.1.6';
+		$this->version = '2.1.7';
 		$this->defaults = array(
 			'currency' => 'USD',
 			'dir_slug' => __('fundraisers','wdf'),
