9ebcac4842fce4df94514ae21605ec842fcedb7c
Jose Jaureguiberry <j.jaureguiberry@gmail.com>
2014-12-14 17:21:13 -0300

    Added subscription details to Recurring column.

--- fundraiser.php
+++ fundraiser.php
@@ -1394,11 +1394,10 @@ class WDF {
 				$reward = (isset($trans['reward'])) ? ' ('.__('Reward: ','wdf').$trans['reward'].')' : '';
 				echo '<a href="'.get_edit_post_link($post->ID).'">'.$this->format_currency( $currency, $trans['gross'] ).$reward.'</a>';
 				break;
-			case 'pledge_recurring' :
-				$trans = $this->get_transaction();
-				if(isset($trans['cycle']))
-					echo $trans['cycle'];
-				break;
+            case 'pledge_recurring' :
+                $trans = $this->get_transaction();
+                echo $this->get_recurring_column_detail( $trans );
+                break;
 			case 'pledge_funder' :
 				$parent = get_post($post->post_parent);
 				echo $parent->post_title;
@@ -1874,6 +1873,71 @@ class WDF {
 
 		return $wdf_pledge_id;
 	}
+
+    /**
+     * Format PayPal's period format to readable text.
+     *
+     * For instance, '1 D' (or 'D' for backward compatibility) will be converted to 'Daily'.
+     *
+     * @since 2.6.1.3
+     * @access public
+     *
+     * @param  string $cycle Cycle code. Expected values are 1 D, 1 W, 1 M, 1 Y.
+     * @return string
+     */
+    function format_cycle( $cycle ){
+        if( !$cycle || empty($cycle)) return '';
+
+        $period = '';
+        switch($cycle){
+            case 'D':
+            case '1 D':
+                $period = __( 'Daily', 'wdf' );
+                break;
+            case 'W':
+            case '1 W':
+                $period = __( 'Weekly', 'wdf' );
+                break;
+            case 'M':
+            case '1 M':
+                $period = __( 'Monthly', 'wdf' );
+                break;
+            case 'Y':
+            case '1 Y':
+                $period = __( 'Yearly', 'wdf' );
+                break;
+        }
+
+        return apply_filters( 'wdf_format_period', $period, $cycle );
+    }
+
+    /**
+     * Parses transaction object and returns the recurring column detail text.
+     *
+     * @since 2.6.1.3
+     * @access public
+     *
+     * @param  array $transaction Transaction object.
+     * @return string
+     */
+    function get_recurring_column_detail( $transaction ){
+        if(!isset($transaction['cycle'])){
+            return 'Not Recurring';
+        } else {
+            $period = $this->format_cycle($transaction['cycle']);
+            $currency = isset( $transaction['currency_code'] ) ? $transaction['currency_code'] : '';
+            $recurring_amount = isset( $transaction['recurring_amount'] ) ? $transaction['recurring_amount'] : '';
+            $detail_text = $this->format_currency( $currency, $recurring_amount ) . ' ' . $period;
+
+            if( isset( $transaction['recurring_transactions'] ) ){
+                $recurring_transactions = $transaction['recurring_transactions'] . ' ' . _n( 'payment completed.', 'payments completed.', $transaction['recurring_transactions'], 'wdf' );
+                $detail_text .= '<br />' . $recurring_transactions;
+            }
+            return $detail_text;
+        }
+
+    }
+
 	function get_transaction($post_id = false) {
 		global $post;
 		if(!$post_id)
